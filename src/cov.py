4afd56af2e65bd6432838213fe4bbe92cdb0fa4e
QijunTong <qijun.tong@uec.ac.jp>
2026-01-13 18:38:04 +0900

    adaptive

--- src/cov.py
+++ src/cov.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 
-def gaussian(arr: np.array) -> np.ndarray:
+def gaussian(arr: np.ndarray) -> np.ndarray:
     gamma = arr[:, np.newaxis] - arr[np.newaxis, :]
     return np.exp(-(gamma**2))
 
 
-def laplacian(arr: np.array) -> np.ndarray:
+def laplacian(arr: np.ndarray) -> np.ndarray:
     gamma = arr[:, np.newaxis] - arr[np.newaxis, :]
     return np.exp(-np.abs(gamma))
+
+
+def generate(lim_l=0, lim_r=50, num=100) -> tuple[np.ndarray, np.ndarray]:
+    t = np.linspace(lim_l, lim_r, num)
+    C_m = gaussian(t)
+    C_n = laplacian(t)
+    return C_m, C_n
--- src/metric.py
+++ src/metric.py
@@ -2,10 +2,14 @@ import numpy as np
 
 
 def wasserstein(A, B) -> np.ndarray:
-    return np.einsum("ij,ij->", A, B)
+    return (
+        np.linalg.trace(A)
+        + np.linalg.trace(B)
+        - 2 * np.sqrt(np.einsum("ij,ij->", A, B))
+    )
 
 
 def kr(A, B) -> np.ndarray:
     L = np.linalg.cholesky(A)
-    M = np.linalg.cholesky(A)
-    return np.einsum("ij,ij", L.T, M)
+    M = np.linalg.cholesky(B)
+    return np.linalg.trace(A) + np.linalg.trace(B) - 2 * np.einsum("ij,ij", L.T, M)
