f929304cdc61be5a5373ce55a6e34205d0dfc2e0
QijunTong <qijun.tong@uec.ac.jp>
2026-01-13 19:58:20 +0900

    ad

--- src/cov.py
+++ src/cov.py
@@ -11,8 +11,12 @@ def laplacian(arr: np.ndarray) -> np.ndarray:
     return np.exp(-np.abs(gamma))
 
 
-def generate(lim_l=0, lim_r=50, num=100) -> tuple[np.ndarray, np.ndarray]:
+def generate(
+    lim_l=0, lim_r=50, num=100, shuffle: bool = False
+) -> tuple[np.ndarray, np.ndarray]:
     t = np.linspace(lim_l, lim_r, num)
+    if shuffle:
+        t = reversed(np.random.shuffle(t))
     C_m = gaussian(t)
     C_n = laplacian(t)
     return C_m, C_n
--- src/metric.py
+++ src/metric.py
@@ -1,15 +1,13 @@
 import numpy as np
+import ot
 
 
 def wasserstein(A, B) -> np.ndarray:
-    return (
-        np.linalg.trace(A)
-        + np.linalg.trace(B)
-        - 2 * np.sqrt(np.einsum("ij,ij->", A, B))
-    )
+    return ot.gaussian.bures_distance(A, B)
 
 
 def kr(A, B) -> np.ndarray:
     L = np.linalg.cholesky(A)
     M = np.linalg.cholesky(B)
-    return np.linalg.trace(A) + np.linalg.trace(B) - 2 * np.einsum("ij,ij", L.T, M)
+    out = np.linalg.norm(np.einsum("ji,ji->i", L, M), ord=1)
+    return np.sqrt(np.linalg.trace(A) + np.linalg.trace(B) - 2 * out)
